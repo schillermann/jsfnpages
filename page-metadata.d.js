cb92e5d756a917e2bb04330c2dcb158958bcf412
Mario Schillermann <mario.schillermann@gmail.com>
2024-11-26 23:09:23 +0100

    add doc to simple page

---
+++ page-metadata.d.js
@@ -0,0 +1,5 @@
+/**
+ * @typedef {Object} pageMetadata
+ * @property {string} name - The name of the metadata
+ * @property {string} value - The value of the metadata
+ */
---
+++ page-output.d.js
@@ -0,0 +1,7 @@
+/** @typedef {import("./page-metadata.d.js").pageMetadata} pageMetadata */
+
+/**
+ * @callback pageOutput
+ * @param {pageMetadata[]} pageMetadata - The metadata for the page.
+ * @returns {pageMetadata[]} The modified metadata for the page
+ */
